898bb196c07e7a0a80f9d32380e7828232a8604a
Daniel-Fonseca-da-Silva <daniel117fonseca@gmail.com>
2025-07-22 15:11:36 +0100

    feat: created model of device

---
+++ src/main/java/com/dafon/device_api/model/Device.java
@@ -0,0 +1,33 @@
+package com.dafon.device_api.model;
+
+import java.time.LocalDateTime;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "devices")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Device {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "brand")
+    private String brand;
+    
+    @Enumerated(EnumType.STRING)
+    @Column(name = "state")
+    private DeviceState state;
+
+    @Column(name = "creation_time")
+    private LocalDateTime creationTime;
+
+}
