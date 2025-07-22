35c811b9401438e2b450c388f01c6cad3345e5db
Daniel-Fonseca-da-Silva <daniel117fonseca@gmail.com>
2025-07-22 18:16:49 +0100

    feat: created exception for not found

---
+++ src/main/java/com/dafon/device_api/exception/NoFoundException.java
@@ -0,0 +1,14 @@
+package com.dafon.device_api.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ProblemDetail;
+
+public class NoFoundException extends CustomException{
+
+    @Override
+    public ProblemDetail toProblemDetail() {
+        var pb = ProblemDetail.forStatus(HttpStatus.NOT_FOUND);
+        pb.setTitle("The field was not found in the database.");
+        return pb;
+    }
+}
