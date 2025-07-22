1f14f9704ca8d9dea36913c8df21b43e731e6a34
Daniel-Fonseca-da-Silva <daniel117fonseca@gmail.com>
2025-07-22 15:11:56 +0100

    feat: created repository of device

---
+++ src/main/java/com/dafon/device_api/repository/DeviceRepository.java
@@ -0,0 +1,11 @@
+package com.dafon.device_api.repository;
+
+import com.dafon.device_api.model.Device;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface DeviceRepository extends JpaRepository<Device, Long> {
+    List<Device> findByBrand(String brand);
+    List<Device> findByState(String state);
+}
