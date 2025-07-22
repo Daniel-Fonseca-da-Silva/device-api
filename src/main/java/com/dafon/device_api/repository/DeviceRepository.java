ef708aa69010fe0cc5bb5f1dba4a0aec0a2749fa
Daniel-Fonseca-da-Silva <daniel117fonseca@gmail.com>
2025-07-22 22:49:37 +0100

    fix: changed String to State on find by state

--- src/main/java/com/dafon/device_api/repository/DeviceRepository.java
+++ src/main/java/com/dafon/device_api/repository/DeviceRepository.java
@@ -1,11 +1,12 @@
 package com.dafon.device_api.repository;
 
 import com.dafon.device_api.model.Device;
+import com.dafon.device_api.model.DeviceState;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
 
 public interface DeviceRepository extends JpaRepository<Device, Long> {
     List<Device> findByBrand(String brand);
-    List<Device> findByState(String state);
+    List<Device> findByState(DeviceState state);
 }
